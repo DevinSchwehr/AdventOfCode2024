639cd5229fa321550466d22a4409bee45e6e642c
Devin Schwehr <dschwehr@mealticket.com>
2024-12-06 12:10:44 -0700

    solved day 5 part two

--- day5/day5.go
+++ day5/day5.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -50,6 +51,55 @@ func partOne(filename string) int {
 	return result
 }
 
+func partTwo(filename string) int {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	pageOrderMap := make(map[int][]int)
+	result := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "|") {
+			orderings := strings.Split(line, "|")
+			keyPage, err1 := strconv.Atoi(orderings[0])
+			valuePage, err2 := strconv.Atoi(orderings[1])
+			if err1 != nil || err2 != nil {
+				log.Fatal("Error getting page orders")
+			}
+			pages := pageOrderMap[keyPage]
+			pages = append(pages, valuePage)
+			pageOrderMap[keyPage] = pages
+		}
+		if strings.Contains(line, ",") {
+			pages := strings.Split(line, ",")
+			var pageNums []int
+			for _, page := range pages {
+				num, err := strconv.Atoi(page)
+				if err != nil {
+					log.Fatal("couldn't parse integer from string")
+				}
+				pageNums = append(pageNums, num)
+			}
+			var incorrect bool
+			incorrect, pageNums = getMiddlePartTwo(pageNums, pageOrderMap)
+			if incorrect {
+				for incorrect {
+					incorrect, pageNums = getMiddlePartTwo(pageNums, pageOrderMap)
+				}
+				result += pageNums[len(pageNums)/2]
+			}
+		}
+	}
+
+	return result
+}
+
 func getMiddle(line string, orderings map[int][]int) int {
 	pages := strings.Split(line, ",")
 	var pageNums []int
@@ -75,6 +125,45 @@ func getMiddle(line string, orderings map[int][]int) int {
 	return pageNums[len(pageNums)/2]
 }
 
+func getMiddlePartTwo(pageNums []int, orderings map[int][]int) (bool, []int) {
+	incorrect := false
+	for index, page := range pageNums {
+		var incorrectIndexes []int
+		//Go through the values for the key
+		for _, value := range orderings[page] {
+			indexes := getPageIndexes(value, pageNums)
+			//Go through the indexes for page
+			for _, pageIndex := range indexes {
+				if pageIndex < index {
+					incorrect = true
+					incorrectIndexes = append(incorrectIndexes, pageIndex)
+				}
+			}
+		}
+		if incorrect {
+			//For any invalid indexes, move that element to be in front of page. Reverse order for proper insertion
+			slices.Reverse(incorrectIndexes)
+			for _, badIndex := range incorrectIndexes {
+				editNums := make([]int, len(pageNums))
+				copy(editNums, pageNums)
+				var reOrder []int
+				elemBefore := editNums[:badIndex]
+				elemBetween := editNums[badIndex+1 : index]
+				elemAfter := editNums[index+1:]
+				reOrder = append(reOrder, elemBefore...)
+				reOrder = append(reOrder, elemBetween...)
+				reOrder = append(reOrder, pageNums[index])
+				reOrder = append(reOrder, pageNums[badIndex])
+				reOrder = append(reOrder, elemAfter...)
+				pageNums = reOrder
+				break
+			}
+		}
+	}
+
+	return incorrect, pageNums
+}
+
 func getPageIndexes(target int, array []int) []int {
 	var indexes []int
 	for index, value := range array {
@@ -83,17 +172,4 @@ func getPageIndexes(target int, array []int) []int {
 		}
 	}
 	return indexes
-}
-
-func partTwo(filename string) int {
-
-	// file, err := os.Open(filename)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer file.Close()
-
-	// scanner := bufio.NewScanner(file)
-
-	return 0
 }
--- day5/day5_test.go
+++ day5/day5_test.go
@@ -10,10 +10,10 @@ func TestPartOne(t *testing.T) {
 	}
 }
 
-// func TestPartTwo(t *testing.T) {
-// 	want := 9
-// 	result := partTwo("test.txt")
-// 	if result != want {
-// 		t.Fatalf(`Did not get expected result. Expected %d, received %d`, want, result)
-// 	}
-// }
+func TestPartTwo(t *testing.T) {
+	want := 123
+	result := partTwo("test.txt")
+	if result != want {
+		t.Fatalf(`Did not get expected result. Expected %d, received %d`, want, result)
+	}
+}
